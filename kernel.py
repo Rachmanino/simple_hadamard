a1ebc0e33cf4bffd33a366547580f664488db7ca
Rachmanino <18805904201@163.com>
2025-04-08 23:59:31 +0800

    init

--- kernel.py
+++ kernel.py
@@ -6,11 +6,13 @@ import math
 import torch
 
 def is_pow_of_2(n):
+    """Check if n is a power of 2."""
     return isinstance(n, int) and n > 0 and (n & (n - 1)) == 0
 
 def hadamard(b, n, dtype):
     """
     A block is responsible for computing one row.
+    The configuration is based on https://github.com/Dao-AILab/fast-hadamard-transform/blob/master/csrc/fast_hadamard_transform_cuda.cu
     """
     assert is_pow_of_2(n), "n must be a power of 2"
     assert 2 <= n <= 32768, "n must be in [2, 32768]"
@@ -27,23 +29,21 @@ def hadamard(b, n, dtype):
     thread_round = int(math.log2(thread_elem))
 
     warps = 1 if threads <= 32 else threads // 32 
-    warp_elem = n // warps 
     warp_round = int(math.log2(threads/warps))
 
     block_round = int(math.log2(warps))
 
-    # assert n * elem_size <= 32 * 1024, "For now we suppose smem can hold all n"
     exchange_round = n * elem_size // 32768 if n * elem_size > 32768 else 1
     thread_elem_in_smem = thread_elem // exchange_round if exchange_round > 1 else thread_elem
 
-    # logging
+    # debug log
     # print(f'{threads=}, {thread_round=}')
     # print(f'{warps=}, {warp_round=}')
     # print(f'{block_round=}')
     
     @T.prim_func
     def main(
-        A: T.Buffer((b, n), dtype),
+        A: T.Buffer((b, n), dtype), # Should be T.Tensor(..) here for TileLang new versions
         B: T.Buffer((b, n), dtype),
     ): 
         with T.Kernel(b, threads=threads) as bx:
@@ -70,7 +70,7 @@ def hadamard(b, n, dtype):
                         local[chunkbase + k] = local[chunkbase + k] + local[chunkbase + k + chunksize // 2]
                         local[chunkbase + k + chunksize // 2] = local[chunkbase + k] - 2 * local[chunkbase + k + chunksize // 2]
 
-            for cur_round in T.serial(exchange_round):
+            for cur_round in T.Pipelined(exchange_round, num_stages=3):
                 exchange_base = thread_elem_in_smem * cur_round
 
                 # 3. Hadamard inside warp, n<=512
@@ -81,7 +81,7 @@ def hadamard(b, n, dtype):
                 for i in T.serial(warp_round):
                     tx_stride = 1 << i
                     sign = (tx >> i) & 1 # get i-th lowest bit of tx, which determines the operation type for shared[tx, :]
-                    for j in T.Pipelined(thread_elem_in_smem, num_stages=2):
+                    for j in T.vectorized(thread_elem_in_smem):
                         shared[tx, j] = T.if_then_else(
                             sign == 0,
                             shared[tx, j] + shared[tx ^ tx_stride, j],
@@ -96,7 +96,7 @@ def hadamard(b, n, dtype):
                 for i in T.serial(block_round):
                     tx_stride = 1 << (warp_round + i)
                     sign = (tx >> (warp_round + i)) & 1 # get i-th lowest bit of tx, which determines the operation type for shared[tx, :]
-                    for j in T.Pipelined(thread_elem_in_smem, num_stages=2):
+                    for j in T.vectorized(thread_elem_in_smem):
                         local[exchange_base+j] = T.if_then_else(
                             sign == 0,
                             local[exchange_base+j] + shared[tx ^ tx_stride, j],
